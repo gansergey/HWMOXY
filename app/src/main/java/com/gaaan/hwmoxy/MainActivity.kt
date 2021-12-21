312b334e05bc4e6b32f8ef30d619ae1cdb01fd33
gaaan <gaaan@outlook.com>
2021-12-21 21:41:39 +0300

    HW4

---
+++ app/src/main/java/com/gaaan/hwmoxy/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.gaaan.hwmoxy
+
+import android.os.Bundle
+import com.gaaan.hwmoxy.databinding.ActivityMainBinding
+import moxy.MvpAppCompatActivity
+import moxy.ktx.moxyPresenter
+
+class MainActivity : MvpAppCompatActivity(), MainView {
+
+    private lateinit var binding: ActivityMainBinding
+    private val presenter by moxyPresenter { MainPresenter() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCalculate.setOnClickListener {
+            presenter.calculate(binding.editText.text.toString())
+        }
+    }
+
+    override fun showResult(result: Long) {
+        binding.resultText.text = result.toString()
+    }
+
+    override fun showError(message: String) {
+        binding.resultText.text = message
+    }
+
+}
